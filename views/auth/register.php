db0f65bd19c6f1fed720c60db37150bae4d6f0e7
Ratana10 <sanratana18@gmail.com>
2024-03-19 22:39:13 +0700

    add image preview

--- views/auth/register.php
+++ views/auth/register.php
@@ -6,6 +6,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
   <title>Register</title>
+  <style>
+    .image-preview {
+      width: 100%;
+      max-height: 300px;
+      object-fit: cover;
+      display: none;
+      border: 3px solid gray;
+      border-radius: 8px;
+      vertical-align: middle;
+    }
+  </style>
 </head>
 
 <body>
@@ -42,10 +53,36 @@
       <div class="form-floating mb-3">
         <input type="file" class="form-control" id="floatingImage" placeholder="Image" name="image">
         <label for="floatingImage">Image</label>
+        <img id="imagePreview" src="#" alt="Image Preview" class="image-preview mt-4">
+
       </div>
       <button class="btn btn-primary mt-4" name="btnRegister" type="submit">Register</button>
     </form>
   </div>
+
+  <!-- jQuery Library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#floatingImage').change(function() {
+        previewImage(this);
+      });
+    });
+
+    function previewImage(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#imagePreview').attr('src', e.target.result).show();
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      } else {
+        $('#imagePreview').hide();
+      }
+    }
+  </script>
 </body>
 
 </html>
