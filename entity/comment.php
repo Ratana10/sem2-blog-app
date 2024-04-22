ef9445f052d0b219853a3311655e5b355acdc84c
Ratana10 <sanratana18@gmail.com>
2024-04-22 19:41:51 +0700

    test comment list

--- entity/comment.php
+++ entity/comment.php
@@ -1,55 +1,80 @@
 <?php
+include_once "user.php";
 
-class Comment{
+class Comment
+{
   private $id;
   private $userId;
   private $postId;
   private $content;
   private $createdAt;
   private $updatedAt;
+  private $user;
 
-  public function __construct($id="", $userId="", $postId="", $content="", $createdAt="", $updatedAt="") {
+  public function __construct($id = "", $userId = "", $postId = "", $content = "", $createdAt = "", $updatedAt = "")
+  {
     $this->setId($id);
     $this->setUserId($userId);
     $this->setPostId($postId);
     $this->setContent($content);
     $this->setCreatedAt($createdAt);
     $this->setUpdatedAt($updatedAt);
+    $this->setUser();
   }
-  public function setId($param){
-    $this->id=$param;
+  public function setId($param)
+  {
+    $this->id = $param;
   }
-  public function getId(){
+  public function getId()
+  {
     return $this->id;
   }
-  public function setUserId($param){
-    $this->userId=$param;
+  public function setUserId($param)
+  {
+    $this->userId = $param;
   }
-  public function getUserId(){
+  public function getUserId()
+  {
     return $this->userId;
   }
-  public function setPostId($param){
-    $this->postId=$param;
+  public function setPostId($param)
+  {
+    $this->postId = $param;
   }
-  public function getPostId(){
+  public function getPostId()
+  {
     return $this->postId;
   }
-  public function setContent($param){
-    $this->content=$param;
+  public function setContent($param)
+  {
+    $this->content = $param;
   }
-  public function getContent(){
+  public function getContent()
+  {
     return $this->content;
   }
-  public function setCreatedAt($param){
-    $this->createdAt=$param;
+  public function setCreatedAt($param)
+  {
+    $this->createdAt = $param;
   }
-  public function getCreatedAt(){
+  public function getCreatedAt()
+  {
     return $this->createdAt;
   }
-  public function setUpdatedAt($param){
-    $this->updatedAt=$param;
+  public function setUpdatedAt($param)
+  {
+    $this->updatedAt = $param;
   }
-  public function getUpdatedAt(){
+  public function getUpdatedAt()
+  {
     return $this->updatedAt;
+  }
+  public function setUser()
+  {
+    $this->user = new User();
+  }
+  public function getUser()
+  {
+    return $this->user;
   }
 }
--- service/commentService.php
+++ service/commentService.php
@@ -28,7 +28,8 @@ class CommentService
     return $this->conn->insert_id;
   }
 
-  public function countComments($postId) {
+  public function countComments($postId)
+  {
     $sql = "SELECT COUNT(*) AS comment_count FROM tbComments WHERE postId = $postId";
 
     $result = $this->conn->query($sql);
@@ -39,5 +40,45 @@ class CommentService
     } else {
       return 0;
     }
+  }
+
+  public function getComments($postId)
+  {
+    $sql = "SELECT c.id, c.userId, c.postId, c.content, c.createdAt, c.updatedAt,  
+            u.id , u.username, u.image
+            FROM tbComments c
+            INNER JOIN tbUsers u
+            ON c.userId = u.id
+            WHERE postId = $postId";
+
+    $result = $this->conn->query($sql);
+
+    if ($result->num_rows === 0) {
+      return false;
+    }
+
+    $comments = array();
+
+    while ($row = $result->fetch_assoc()) {
+      $comment = $this->fetchToComment($row);
+      $comments[] = $comment;
+    }
+
+    return $comments;
+  }
+
+  private function fetchToComment($row)
+  {
+    $comment = new Comment(
+      $row['id'],
+      $row['userId'],
+      $row['postId'],
+      $row['content'],
+      $row['createdAt'],
+      $row['updatedAt'],
+    );
+    $comment->getUser()->setUsername($row['username']);
+    $comment->getUser()->setImage($row['image']);
+    return $comment;
   }
 }
